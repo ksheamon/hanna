79906a7c5c00c6bed7d335665ca05c25a295a76c
Kimberly Sheamon <ksheamon@bluefountainmedia.com>
2018-05-19 17:16:30 -0400

    restructure for splash

--- hannashints/wp-content/themes/hannashints/footer.php
+++ hannashints/wp-content/themes/hannashints/footer.php
@@ -30,6 +30,6 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-
+<script type="text/javascript" src="https://cdn.ywxi.net/js/1.js" async></script>
 </body>
 </html>
--- splash/index.php
+++ splash/index.php
@@ -65,5 +65,6 @@
 	</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script type='text/javascript' src='./assets/main.js'></script>
+	<script type="text/javascript" src="https://cdn.ywxi.net/js/1.js" async></script>
 </body>
 </html>
